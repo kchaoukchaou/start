affab35c006a0c345c4ed286f31576ffed38ae80
kchaou <kchaouyassine93@gmail.com>
2018-11-14 16:22:34 +0100

    first bundle creation

---
+++ src/FirstBundle/Entity/Blog.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace FirstBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Blog
+ *
+ * @ORM\Table(name="blog")
+ * @ORM\Entity(repositoryClass="FirstBundle\Repository\BlogRepository")
+ */
+class Blog
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="cin", type="integer")
+     */
+    private $cin;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Blog
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set cin
+     *
+     * @param integer $cin
+     *
+     * @return Blog
+     */
+    public function setCin($cin)
+    {
+        $this->cin = $cin;
+
+        return $this;
+    }
+
+    /**
+     * Get cin
+     *
+     * @return int
+     */
+    public function getCin()
+    {
+        return $this->cin;
+    }
+}
+
